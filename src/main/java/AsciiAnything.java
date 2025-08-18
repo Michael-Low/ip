9e8da750845c729a56fee4bdfdcdae6a6efd6490
Michael Low <mike@lows.sg>
2025-08-18 13:48:47 +0800

    rename bot to AsciiAnything

---
+++ src/main/java/AsciiAnything.java
@@ -0,0 +1,9 @@
+public class AsciiAnything {
+    public static void main(String[] args) {
+        String welcomeText = "Hello! I'm AsciiAnything\n" +
+                "What can I do for you?\n" +
+                "---------------------------\n" +
+                "Bye. Hope to see you again soon!\n";
+        System.out.println(welcomeText);
+    }
+}
