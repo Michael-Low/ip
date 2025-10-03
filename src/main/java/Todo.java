7ec47be03d8c7dd63eff51a4fa882f16ac7e5034
Michael Low <mike@lows.sg>
2025-10-03 11:20:11 +0800

    add toSaveFormat method for tasks and subclasses

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -2,8 +2,18 @@ public class Todo extends Task {
     Todo(String desc) {
         super(desc);
     }
+
+    Todo(String desc, boolean done) {
+        super(desc, done);
+    }
+
     @Override
     public String toString() {
         return "[T]" + super.toString();
+    }
+
+    @Override
+    public String toSaveFormat() {
+        return "T|" + super.toSaveFormat();
     }
 }
